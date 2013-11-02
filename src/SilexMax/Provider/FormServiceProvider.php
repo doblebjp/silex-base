f0187b5a3ea866700dffdb41c1c44f48b1b2ffa0
Ben <benedict@phpdevel.net>
2013-11-02 16:18:41 +0100

    Unique entity validator

--- src/SilexMax/Provider/FormServiceProvider.php
+++ src/SilexMax/Provider/FormServiceProvider.php
@@ -7,15 +7,26 @@ use Silex\ServiceProviderInterface;
 use Silex\Provider\FormServiceProvider as SilexFormServiceProvider;
 use Silex\Provider\ValidatorServiceProvider;
 use Symfony\Bridge\Doctrine\Form\DoctrineOrmExtension;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntityValidator;
 use Dominikzogg\Silex\Provider\DoctrineOrmManagerRegistryProvider as ManagerRegistryServiceProvider;
 
 class FormServiceProvider implements ServiceProviderInterface
 {
     public function register(Application $app)
     {
-        $app->register(new SilexFormServiceProvider());
-        $app->register(new ValidatorServiceProvider());
         $app->register(new ManagerRegistryServiceProvider());
+
+        $app->register(new ValidatorServiceProvider(), [
+            'validator.validator_service_ids' => [
+                'doctrine.orm.validator.unique' => 'silex_max.unique_entity_validator'
+            ]
+        ]);
+
+        $app['silex_max.unique_entity_validator'] = $app->share(function () use ($app) {
+            return new UniqueEntityValidator($app['doctrine']);
+        });
+
+        $app->register(new SilexFormServiceProvider());
 
         $app['form.extensions'] = $app->share($app->extend('form.extensions', function ($extensions, $app) {
             $extensions[] = new DoctrineOrmExtension($app['doctrine']);
